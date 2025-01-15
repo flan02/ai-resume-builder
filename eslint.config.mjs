7b2a7fc7ec9c40807b2fb93a4d2ed65567bf10cc
flan02 <chanivetdan@hotmail.com>
2025-01-15 11:35:13 -0300

    mend
    Adding some rules to eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,11 +13,17 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
     rules: {
+      // "@typescript-eslint/no-unused-vars": [
+      //   "warn",
+      //   {
+      //     argsIgnorePattern: "^_",
+      //     varsIgnorePattern: "^_",
+      //   },
+      // ],
       "@typescript-eslint/no-unused-vars": [
-        "warn",
+        "error",
         {
-          argsIgonorePattern: "^_",
-          varsIgnorePattern: "^_",
+          ignoreSiblings: "true",
         },
       ],
     },
